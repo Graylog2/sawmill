5abe541ee2e7e2f90f1ba70b328151577dd48482
Linus Pahl <linus@graylog.com>
2023-09-29 09:11:46 +0200

    Fixing styling for ace editor component. (#94)

--- lib/styled-components/component-styles/aceEditor.ts
+++ lib/styled-components/component-styles/aceEditor.ts
@@ -18,64 +18,64 @@
 import { StyledComponentsTheme } from '../types';
 
 const aceEditorStyles = (colors: StyledComponentsTheme['colors']) => `
-  .ace_editor {
-    border: 1px solid var(--colors-gray-80);
+ .ace_editor {
+    border: 1px solid ${colors.gray[80]};
     border-radius: 5px;
   }
-  
+
   .ace-graylog {
-    background-color: var(--colors-global-contentBackground);
-    color: var(--colors-global-contentBackground);
-  
+    background-color: ${colors.global.contentBackground};
+    color: ${colors.variant.darkest.default};
+
     &.ace_multiselect .ace_selection.ace_start {
-      box-shadow: 0 0 3px 0 var(--colors-global-contentBackground);
+      box-shadow: 0 0 3px 0 ${colors.global.contentBackground};
     }
-  
+
     .ace_gutter {
-      background: var(--colors-variant-lightest-default);
-      color: var(--colors-global-contentBackground);
+      background: ${colors.variant.lightest.default};
+      color: ${colors.variant.darkest.default};
     }
-  
+
     .ace_print-margin {
       width: 1px;
-      background: var(--colors-variant-lightest-default);
+      background: ${colors.variant.lightest.default};
     }
-  
+
     .ace_cursor {
-      color: var(--colors-variant-light-default);
+      color: ${colors.variant.light.default};
     }
-  
+
     .ace_marker-layer {
       .ace_selection {
-        background: var(--colors-gray-70);
+        background: ${colors.gray[70]};
       }
-  
+
       .ace_step {
-        background: rgb(255, 255, 0);
+        background: rgb(255 255 0);
       }
-  
+
       .ace_bracket {
         margin: -1px 0 0 -1px;
-        border: 1px solid var(--colors-gray-70);
+        border: 1px solid ${colors.gray[70]};
       }
-  
+
       .ace_selected-word {
         border: 1px solid ${colors.gray[60]};
       }
-  
+
       .ace_active-line {
-        background: var(--colors-variant-lighter-default);
+        background: ${colors.variant.lighter.default};
       }
     }
-  
+
     .ace_gutter-active-line {
-      background-color: var(--colors-variant-lighter-default);
+      background-color: ${colors.variant.lighter.default};
     }
-  
+
     .ace_invisible {
-      color: var(--colors-gray-70);
+      color: ${colors.gray[70]};
     }
-  
+
     .ace_keyword,
     .ace_meta,
     .ace_storage,
@@ -83,56 +83,56 @@ const aceEditorStyles = (colors: StyledComponentsTheme['colors']) => `
     .ace_support.ace_type {
       color: ${colors.global.link};
     }
-  
+
     .ace_keyword.ace_operator {
       color: ${colors.variant.darker.info};
     }
-  
+
     .ace_constant.ace_character,
     .ace_constant.ace_language,
     .ace_constant.ace_numeric,
     .ace_keyword.ace_other.ace_unit,
     .ace_support.ace_constant,
     .ace_variable.ace_parameter {
-      color: var(--colors-variant-darker-primary);
+      color: ${colors.variant.darker.primary};
     }
-  
+
     .ace_constant.ace_other {
-      color: var(--colors-variant-darker-default);
+      color: ${colors.variant.darker.default};
     }
-  
+
     .ace_invalid {
-      color: var(--colors-global-altText);
+      color: ${colors.global.textAlt};
       background-color: ${colors.variant.light.danger};
     }
-  
+
     .ace_invalid.ace_deprecated {
-      color: var(--colors-global-altText);
-      background-color: var(--colors-variant-primary);
+      color: ${colors.global.textAlt};
+      background-color: ${colors.variant.primary};
     }
-  
+
     .ace_fold {
       background-color: ${colors.variant.info};
-      border-color: var(--colors-variant-darkest-default);
+      border-color: ${colors.variant.darkest.default};
     }
-  
+
     .ace_entity.ace_name.ace_function,
     .ace_support.ace_function,
     .ace_variable {
       color: ${colors.variant.info};
     }
-  
+
     .ace_support.ace_class,
     .ace_support.ace_type {
       color: ${colors.variant.darker.warning};
     }
-  
+
     .ace_heading,
     .ace_markup.ace_heading,
     .ace_string {
       color: ${colors.variant.darker.success};
     }
-  
+
     .ace_entity.ace_name.ace_tag,
     .ace_entity.ace_other.ace_attribute-name,
     .ace_meta.ace_tag,
@@ -140,7 +140,7 @@ const aceEditorStyles = (colors: StyledComponentsTheme['colors']) => `
     .ace_variable {
       color: ${colors.variant.light.danger};
     }
-  
+
     .ace_comment {
       color: ${colors.variant.dark.default};
     }
