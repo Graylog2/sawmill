526ac96f417deeb65cc4bb606b7c79667e99bb74
Linus Pahl <linus@graylog.com>
2024-03-22 09:03:01 +0100

    Improve table state colors.

--- lib/styled-components/utils/tableColors.ts
+++ lib/styled-components/utils/tableColors.ts
@@ -1,6 +1,5 @@
 import { COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT } from '../../THEME_BASE';
 import { StyledComponentsTheme } from '../types';
-import { darken, lighten } from '../../utils/colors';
 import { ColorScheme } from '../../types';
 import { MantineColors } from '../../mantine/types';
 import { opacify } from '../../utils';
@@ -9,8 +8,6 @@ const tableColors = (colorScheme: ColorScheme, completeVariant: StyledComponents
   if (![COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT].includes(colorScheme)) {
     throw new Error(`Requires "${COLOR_SCHEME_DARK}" or "${COLOR_SCHEME_LIGHT}" color scheme option.`);
   }
-
-  const adjust = colorScheme === COLOR_SCHEME_DARK ? lighten : darken;
 
   return {
     head: {
@@ -23,16 +20,16 @@ const tableColors = (colorScheme: ColorScheme, completeVariant: StyledComponents
       border: colorScheme === COLOR_SCHEME_LIGHT ? opacify(colors.gray[1], 0.75) : opacify(colors.gray[1], 0.20),
     },
     variant: {
-      danger: adjust(completeVariant.danger as string, 0.75),
-      active: adjust(completeVariant.default as string, 0.75),
-      info: adjust(completeVariant.info as string, 0.75),
-      primary: adjust(completeVariant.primary as string, 0.75),
-      success: adjust(completeVariant.success as string, 0.75),
-      warning: adjust(completeVariant.warning as string, 0.75),
+      danger: completeVariant.light.danger,
+      active: opacify(colors.gray[2], 0.2),
+      info: completeVariant.light.info,
+      primary: completeVariant.light.primary,
+      success: completeVariant.light.success,
+      warning: completeVariant.light.warning,
     },
     variantHover: {
       danger: completeVariant.lighter.danger,
-      active: completeVariant.lighter.default,
+      active: opacify(colors.gray[2], 0.2),
       info: completeVariant.lighter.info,
       primary: completeVariant.lighter.primary,
       success: completeVariant.lighter.success,
