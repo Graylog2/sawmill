434580adb045acacc14460047509f08da823975f
Linus Pahl <linus.pahl@graylog.com>
2025-07-17 11:17:51 +0200

    cleanup

--- lib/utils/readableColor.ts
+++ lib/utils/readableColor.ts
@@ -6,6 +6,8 @@ const readableColor = (lightTextColor: string, darkTextColor: string) => (hex: s
   /**
    * Returns either the light or dark text color for the best contrast, depending on the luminosity of the given color. Follows [W3C specs for readability](https://www.w3.org/TR/WCAG20-TECHS/G18.html).
    *
+   * @param {string} lightTextColor - the primary light text color of the currently selected theme
+   * @param {string} darkTextColor - the primary dark text color of the currently selected theme
    * @param {string} color - any string that represents a color (ex: "#f00" or "rgb(255, 0, 0)")
    */
 
