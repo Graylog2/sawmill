f1123a7949c2481205da4bdb871dccc050ac9683
Linus Pahl <linus.pahl@graylog.com>
2026-04-24 09:18:04 +0200

    Properly handle transparent color.

--- lib/utils/flattenColorStack.ts
+++ lib/utils/flattenColorStack.ts
@@ -1,18 +1,19 @@
 import chroma, { Color } from 'chroma-js';
 
-const flattenColorStack = (colors: Array<Color>) => {
-  if (colors.length === 0) {
+const flattenColorStack = (colors: Array<string | Color>) => {
+  const filteredColors = colors.filter((color) => color !== 'transparent').map((color) => chroma(color));
+
+  if (filteredColors.length === 0) {
     throw new Error('flattenColorStack requires at least one color.');
   }
 
-  return colors
+  return filteredColors
     .reduce((acc, color) => {
-      const current = chroma(color);
-      const alpha = current.alpha();
+      const alpha = color.alpha();
 
       return chroma.mix(
         acc,
-        current.alpha(1),
+        color.alpha(1),
         alpha,
         'rgb',
       );
