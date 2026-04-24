acfd6842dbd3d26db0c3785675569e0775f41135
Linus Pahl <linus.pahl@graylog.com>
2026-04-24 09:19:04 +0200

    Describe what `flattenColorStack` does.

--- lib/utils/flattenColorStack.ts
+++ lib/utils/flattenColorStack.ts
@@ -1,5 +1,6 @@
 import chroma, { Color } from 'chroma-js';
 
+// Flattens layered colors into a single opaque hex value by compositing them in order.
 const flattenColorStack = (colors: Array<string | Color>) => {
   const filteredColors = colors.filter((color) => color !== 'transparent').map((color) => chroma(color));
 
