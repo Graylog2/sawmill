11e9e41890a5eb15c48999f0b1f73f5b66924cdd
Linus Pahl <46300478+linuspahl@users.noreply.github.com>
2026-07-28 10:24:26 +0200

    Change table header color in dark mode. (#213)

--- lib/styled-components/utils/tableColors.ts
+++ lib/styled-components/utils/tableColors.ts
@@ -1,12 +1,19 @@
-import { COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT, COLOR_WHITE } from '../../THEME_BASE';
-import { StyledComponentsTheme } from '../types';
-import { ColorScheme } from '../../types';
-import { MantineColors } from '../../mantine/types';
-import { opacify } from '../../utils';
+import { COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT, COLOR_WHITE } from "../../THEME_BASE";
+import { StyledComponentsTheme } from "../types";
+import { ColorScheme } from "../../types";
+import { MantineColors } from "../../mantine/types";
+import { opacify } from "../../utils";
 
-const tableColors = (colorScheme: ColorScheme, completeVariant: StyledComponentsTheme['colors']['variant'], global: StyledComponentsTheme['colors']['global'], colors: MantineColors) => {
+const tableColors = (
+  colorScheme: ColorScheme,
+  completeVariant: StyledComponentsTheme["colors"]["variant"],
+  global: StyledComponentsTheme["colors"]["global"],
+  colors: MantineColors,
+) => {
   if (![COLOR_SCHEME_DARK, COLOR_SCHEME_LIGHT].includes(colorScheme)) {
-    throw new Error(`Requires "${COLOR_SCHEME_DARK}" or "${COLOR_SCHEME_LIGHT}" color scheme option.`);
+    throw new Error(
+      `Requires "${COLOR_SCHEME_DARK}" or "${COLOR_SCHEME_LIGHT}" color scheme option.`,
+    );
   }
 
   const isLightTheme = colorScheme === COLOR_SCHEME_LIGHT;
@@ -16,9 +23,9 @@ const tableColors = (colorScheme: ColorScheme, completeVariant: StyledComponents
       background: isLightTheme ? opacify(colors.gray[1], 0.45) : opacify(colors.gray[4], 0.5),
     },
     row: {
-      background: 'transparent',
-      backgroundStriped: isLightTheme ? opacify(colors.gray[0], 0.4) : opacify(colors.gray[4], 0.5),
-      backgroundHover: isLightTheme ? opacify(colors.gray[1], 0.4) : colors.gray[4],
+      background: "transparent",
+      backgroundStriped: isLightTheme ? opacify(colors.gray[0], 0.4) : opacify(colors.gray[4], 0.2),
+      backgroundHover: isLightTheme ? opacify(colors.gray[1], 0.5) : colors.gray[4],
       backgroundExpanded: isLightTheme ? opacify(colors.gray[0], 0.75) : colors.gray[8],
       divider: isLightTheme ? colors.gray[1] : opacify(colors.gray[4], 0.9),
     },
